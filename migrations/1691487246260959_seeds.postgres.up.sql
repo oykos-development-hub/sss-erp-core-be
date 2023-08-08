520c173060de40b9be02b5d63fa1a79c1a8203d7
oykos-development-hub <hub@oykos.me>
2023-08-08 12:45:24 +0000

    Update code from GitLab

--- migrations/1691487246260959_seeds.postgres.up.sql
+++ migrations/1691487246260959_seeds.postgres.up.sql
@@ -17,21 +17,39 @@ INSERT INTO users (
     (6, 'Igor', 'Milošević', 'igor.milosevic@example.com', NULL, TRUE, 'zaposleni123', '1234', '382-67-1617181', TRUE, FALSE, NULL, '2023-08-08 17:00:00', '2023-08-08 17:00:00', 2); -- Adjust the role_id as needed
 
 -- engagements for 5 users
-INSERT INTO settings (
-    id, title, abbreviation, entity, description, value, color, icon, created_at, updated_at
-) VALUES
+INSERT INTO settings 
+    (id, title, abbreviation, entity, description, value, color, icon, created_at, updated_at) 
+VALUES
     (1, 'Sudija', 'SUD', 'engagement_types', 'Angažman kao sudija u pravosudnom sistemu.', 'sudija', '#D32F2F', 'judge_icon.png', '2023-08-08 17:00:00', '2023-08-08 17:00:00'),
     (2, 'Tužilac', 'TUZ', 'engagement_types', 'Angažman kao tužilac u pravosudnom sistemu.', 'tuzilac', '#1976D2', 'prosecutor_icon.png', '2023-08-08 18:00:00', '2023-08-08 18:00:00'),
     (3, 'Advokat', 'ADV', 'engagement_types', 'Angažman kao advokat u pravosudnom sistemu.', 'advokat', '#388E3C', 'lawyer_icon.png', '2023-08-08 19:00:00', '2023-08-08 19:00:00'),
     (4, 'Pravni Sekretar', 'PSK', 'engagement_types', 'Angažman kao pravni sekretar u pravosudnom sistemu.', 'pravni_sekretar', '#8E24AA', 'legal_secretary_icon.png', '2023-08-08 21:00:00', '2023-08-08 21:00:00');
     (5, 'IT Stručnjak', 'ITS', 'engagement_types', 'Angažman kao IT stručnjak u pravosudnom sistemu.', 'it_strucnjak', '#7B1FA2', 'it_expert_icon.png', '2023-08-08 22:00:00', '2023-08-08 22:00:00');
 
+-- contract types
+INSERT INTO settings 
+    (id, title, abbreviation, entity, description, value, color, icon, created_at, updated_at) 
+VALUES
+    (6,'Ugovor na odredjeno vrijeme', 'UOV', 'contract_types', 'Ugovor na odredjeno vrijeme', NULL, NULL, NULL, NOW(), NOW()),
+    (7,'Ugovor na neodredjeno vrijeme', 'UNV', 'contract_types', 'Ugovor o radu', NULL, NULL, NULL, NOW(), NOW())
+
+
+-- resolution types
+INSERT INTO settings (
+    id, title, abbreviation, entity, description, value, color, icon, created_at, updated_at
+) VALUES
+    (8, 'Rešenje', 'RŠ', 'resolution_types', 'Rešavanje problema na konkretnom radnom mestu.', NULL, '#006699', 'fa fa-file-text-o', NOW(), NOW()),
+	(9, 'Odluka', 'OD', 'resolution_types', 'Donošenje odluke o promeni radnog mesta, povećanju plata.', NULL, '#006699', 'fa fa-file-text-o', NOW(), NOW()),
+	(10, 'Resavanje problema', 'RP', 'resolution_types', 'Rešavanje problema na radnom mestu.', NULL, '#006699', 'fa fa-file-text-o', NOW(), NOW())
+
+-- education types
 INSERT INTO settings (
     id, title, abbreviation, entity, description, created_at, updated_at)
 VALUES
-    ('Bachelor of Law', 'LLB', 'education_types', 'A basic degree in law and legal studies', NOW(), NOW()),
-    ('Master of Law', 'LLM', 'education_types', 'An advanced, postgraduate academic degree in law', NOW(), NOW()),
-    ('Doctor of Law', 'JD', 'education_types', 'A professional doctorate and first professional graduate degree in law', NOW(), NOW()),
-    ('Master of Criminal Justice', 'MCJ', 'education_types', 'A postgraduate degree focusing on the study of criminal justice and criminology', NOW(), NOW()),
-    ('Paralegal Certificate', 'PC', 'education_types', 'A certificate program in legal studies for paralegals', NOW(), NOW()),
-    ('Ph.D. in Legal Studies', 'PhD', 'education_types', 'A doctoral degree focusing on research and academic approach to law and legal studies', NOW(), NOW());
+    (11, 'Nivo 1 - Osnovna stručna osposobljenost', '1', 'education_types', 'opis', NOW(), NOW()),
+    (12, 'Nivo 2 - Srednja stručna osposobljenost', '2', 'education_types', 'opis', NOW(), NOW()),
+    (13, 'Nivo 3 - Srednja stručna osposobljenost s dodatnim obrazovanjem', '3', 'education_types', 'opis', NOW(), NOW()),
+    (14, 'Nivo 4 - Visoka stručna sprema', '4', 'education_types', 'opis', NOW(), NOW()),
+    (15, 'Nivo 5 - Diplomske studije (prva i druga godina)', '5', 'education_types', 'opis', NOW(), NOW()),
+    (16, 'Nivo 6 - Bachelor diploma', '6', 'education_types', 'opis', NOW(), NOW()),
+    (17, 'Nivo 7 - Postdiplomski studiji', '7', 'education_types', 'opis', NOW(), NOW());
