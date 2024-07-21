6d67e03a405c4b7f8ac57c0a7f308ec799804133
oykos-development-hub <hub@oykos.me>
2024-07-21 12:15:51 +0000

    Update code from GitLab

---
+++ migrations/1721563536681872_fix-permissions.postgres.up.sql
@@ -0,0 +1,5 @@
+update permissions set path = '/finance/deposit/transit' where id = 34;
+update permissions set path = '/finance/deposit/transit/initial-state' where id = 50;
+update permissions set path = '/finance/deposit/transit/payment' where id = 51;
+update permissions set path = '/finance/deposit/transit/tax-contribution-calculation' where id = 52;
+update permissions set path = '/finance/deposit/transit/payment-orders' where id = 53;
